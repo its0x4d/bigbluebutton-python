05098d9f89ce6a0fbe3868ea6673e5128317af24
Mostafa Mosavi <mostafa.uwsgi@gmail.com>
2020-05-20 05:46:30 +0430

    Project Started

--- examples/getMeetings.py
+++ examples/getMeetings.py
@@ -1,7 +1,1 @@
-from bbb_monitor import MainBBB
-
-
-bbb = MainBBB('https://live.youmeet.ir/bigbluebutton/api/', 'wgYvfg2bzzeYlNmZdhfOlYXLQXT4bLZ8a5zDbqvyjGg')
-
-s = bbb.send_request('getMeetings', 1)
-print(s)
+# Bye
