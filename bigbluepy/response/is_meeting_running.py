1d7ef06e85ec6efe91a2ffe7ab1dfd33c627d105
Mostafa Mosavi <mosydev2016@gmail.com>
2022-05-22 17:27:48 +0430

    created new files

--- bigbluepy/response/is_meeting_running.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class isMeetingRunningResponse:
-    returncode: str = None
-    messageKey: str = None
-    message: str = None
-    running: bool = None
